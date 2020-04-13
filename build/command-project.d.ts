5eed127501060fc35455fd8b006fdce6ffa941a9
James-Alex Matthews <vamtiger@vamtiger.com>
2020-04-13 03:38:43 +0200

    vamtiger-node-typescript-commit: cleaned bundle

--- build/command-project.d.ts
+++
@@ -1,1 +0,0 @@
-export default function (): Promise<import("./types").Result> | undefined;
--- build/project.d.ts
+++
@@ -1,2 +0,0 @@
-import { IProject, Result } from './types';
-export default function (params: IProject): Promise<Result>;
